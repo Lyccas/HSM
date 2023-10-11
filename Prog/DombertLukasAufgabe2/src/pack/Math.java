6c38f3da55fe2b8def6e9028b3d0705a8abf2829
Lukas Dombert <82817950+Lukas21712@users.noreply.github.com>
2023-10-11 10:38:21 +0200

    Aufgabe 2
    Aufgabe 2 angefangen

---
+++ Prog/DombertLukasAufgabe2/src/pack/Math.java
@@ -0,0 +1,17 @@
+package pack;
+
+import java.util.Random;
+
+public class Math {
+    public static double getRandomNumber() {
+        Random random = new Random();
+        return random.nextDouble();
+    }
+
+    public static void checkWinner() {
+        boolean winner = false;
+        while (!winner) {
+            
+        }
+    }
+}
