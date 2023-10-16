853e34a60b3f4197acffc0fc851c937cf7400c82
Lukas Dombert <82817950+Lukas21712@users.noreply.github.com>
2023-10-16 13:23:07 +0200

    Aufgabe 1

---
+++ Prog/DombertLukasAufgabe1/src/pack/ChechHCN2.java
@@ -0,0 +1,28 @@
+package pack;
+
+public class ChechHCN2 {
+    public static int getFactors(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int factors = 1;
+        for (int i = 2; i <= n; i++) {
+            if (n % i == 0) {
+                factors++;
+            }
+        }
+
+        return factors;
+    }
+
+    public static boolean isHCN(int n) {
+        for (int i = 1; i < n; i++) {
+            if (getFactors(i) >= getFactors(n)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
