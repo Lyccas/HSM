dddcbd27e2601ba434df4c92453d8605bec937d9
Lukas Dombert <82817950+Lukas21712@users.noreply.github.com>
2023-11-01 09:13:10 +0100

    Aufgabe 1

--- Prog/DombertLukasAufgabe1/src/pack/Main.java
+++ Prog/DombertLukasAufgabe1/src/pack/Main.java
@@ -8,11 +8,11 @@ package pack;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(HighlyComposite.isHCN(840));
-        System.out.println(HighlyComposite.isHCN(843));
-        System.out.println("-".repeat(50));
+//        System.out.println(HighlyComposite.isHCN(840));
+//        System.out.println(HighlyComposite.isHCN(843));
+//        System.out.println("-".repeat(50));
         System.out.println(HCNList.getHCNList(13));
-        System.out.println("-".repeat(50));
-        System.out.println(AnalyzeFactor.analyzeFactors(840));
+//        System.out.println("-".repeat(50));
+//        System.out.println(AnalyzeFactor.analyzeFactors(840));
     }
 }
