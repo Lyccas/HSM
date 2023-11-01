344929c0bf174042045e3ab82c9a851da24d8574
Lukas Dombert <82817950+Lukas21712@users.noreply.github.com>
2023-11-01 09:13:26 +0100

    Aufgabe 1

--- Prog/SirnameFirstnameAufgabe1/src/pack/HighlyComposite.java
+++ Prog/SirnameFirstnameAufgabe1/src/pack/HighlyComposite.java
@@ -1,4 +1,27 @@
 package pack;
 
 public class HighlyComposite {
+    public static String analyzeFactors(int n) {
+        int factor = 2;
+        // Startfaktor festlegen
+
+        System.out.println("Analyze factors for " + n);
+
+        while (n > 1) {
+            // Schleife soll so lange laufen, wie n groesser als 1 ist
+            while (n % factor == 0) {
+                // Schleife soll so lange laufen, wie n noch durch denselben Faktor teilbar ist
+                n = n / factor;
+                /*
+                 * Hier wird der alte Wert von n mit dem neuen Wert von n ueberschrieben,
+                 * da hier die Zahl n durch den Faktor geteilt wird
+                 */
+                return "Factor: " + factor + ", current value after div: " + n;
+                // Hier wird die Ausgabe erstellt
+            }
+            factor++;
+            // Faktor wird hochgezaehlt
+        }
+        return null;
+    }
 }
--- Prog/SirnameFirstnameAufgabe1/src/pack/Main.java
+++ Prog/SirnameFirstnameAufgabe1/src/pack/Main.java
@@ -2,6 +2,6 @@ package pack;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(HCNList.getHCNList(840));
     }
 }
