6badb360180afa44b1fe7a0dc4215213fcf5d643
Lukas Dombert <82817950+Lukas21712@users.noreply.github.com>
2023-11-01 09:13:18 +0100

    Aufgabe 2

--- Prog/DombertLukasAufgabe2/src/pack/Simulation.java
+++ Prog/DombertLukasAufgabe2/src/pack/Simulation.java
@@ -2,6 +2,6 @@ package pack;
 
 public class Simulation {
     public static void main(String[] args) {
-        Game.simulationWinningRate(0.7, 0.4, 100000);
+        System.out.println(Game.simulation(0.7, 0.4, 100000));
     }
 }
